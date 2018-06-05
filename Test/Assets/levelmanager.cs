535ab01c3cf3759fadde8997fbb05a367c6ef9a4
Ollie <clamptamp@gmail.com>
2018-06-05 23:07:08 +1000

    Added Assets

---
+++ Test/Assets/levelmanager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelmanager : MonoBehaviour {
+
+	public void LoadLevel (string name){
+		Application.LoadLevel(name);
+	}
+
+	public void quitrequest (){
+		Application.Quit ();
+	}
+
+}
