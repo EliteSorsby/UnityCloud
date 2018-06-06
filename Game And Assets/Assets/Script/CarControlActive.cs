d438022c90cec58c2d528ee3b40e066fc16ddb18
Ollie <clamptamp@gmail.com>
2018-06-07 00:11:37 +1000

    New Stuff

---
+++ Game And Assets/Assets/Script/CarControlActive.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+
+    public class CarControlActive : MonoBehaviour
+    {
+
+        public GameObject CarControl;
+
+        void Start() {
+
+            CarControl.GetComponent<CarController>().enabled = true;
+        }
+    }
+
+
